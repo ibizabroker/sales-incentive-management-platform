f7bc2953d62b27c54c5af6620aee06d25417acdb
tarun gowda <tarun.gowda2000@gmail.com>
2022-07-16 13:22:11 +0530

    initial commit

---
+++ simp-backend/src/main/java/com/company/simpbackend/SimpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.company.simpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpBackendApplication.class, args);
+	}
+
+}
---
+++ simp-backend/src/test/java/com/company/simpbackend/SimpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.simpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
