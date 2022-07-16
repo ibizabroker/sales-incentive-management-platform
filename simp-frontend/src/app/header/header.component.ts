f8f3c79e1af79829ddf3d0fbed57b24468010e8e
tarun gowda <tarun.gowda2000@gmail.com>
2022-07-16 20:46:19 +0530

    header

--- simp-frontend/src/app/header/header.component.ts
+++ simp-frontend/src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserAuthService } from '../_service/user-auth.service';
+import { UserService } from '../_service/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private userAuthService: UserAuthService, 
+    private router: Router,
+    public userService: UserService,
+  ) { }
 
+  name = this.userAuthService.getName();
   ngOnInit(): void {
+  }
+
+  public isLoggedIn() {
+    console.log(this.name);
+    return this.userAuthService.isLoggedIn();
+  }
+
+  public logout() {
+    this.userAuthService.clear();
+    this.router.navigate(['/']);
   }
 
 }
