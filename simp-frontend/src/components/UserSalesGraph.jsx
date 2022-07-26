a09e9fe54b17873b247395af27cb1802e586733a
kumarsaksham <64663891+kumarsaksham@users.noreply.github.com>
2022-07-26 21:54:08 +0530

    changes in components

---
+++ simp-frontend/src/components/UserSalesGraph.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import UserNavbar from './UserNavbar'
+
+export default class UserSalesGraph extends Component {
+  render() {
+    return (
+        <div>
+            <UserNavbar/>
+            <div>
+                UserSalesGraph
+            </div>
+        </div>
+    )
+  }
+}
