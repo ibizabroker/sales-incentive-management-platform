7cdfa8bd68e8f045c742729ba47c78bb34c29891
prateeksIngh508 <prateek.singh508@gmail.com>
2022-07-26 15:29:39 +0530

    added products and product type

---
+++ simp-backend/src/main/java/com/company/simpbackend/entity/ProductType.java
@@ -0,0 +1,18 @@
+package com.company.simpbackend.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class ProductType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer pTypeId;
+    private String pType;
+
+}
