6bb939bfa31433dd71e77db4e34a12187b70b9ff
kumarsaksham <64663891+kumarsaksham@users.noreply.github.com>
2022-07-26 21:53:32 +0530

    added routing

--- simp-frontend/src/App.js
+++ simp-frontend/src/App.js
@@ -1,19 +1,29 @@
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes ,Route,} from 'react-router-dom';
+
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
+import LogIn from './components/LogIn';
+import UserNavbar from './components/UserNavbar';
 import SalesComponent from './components/SalesComponent';
+import Usersales from './components/Usersales';
+import UserSalesGraph from './components/UserSalesGraph';
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <HeaderComponent/>
-      <div className="container">
-        <SalesComponent />
-      </div>
+    <Router>
+      <HeaderComponent />
       
+      <Routes>
+        <Route path='/' exact element={<LogIn/>} />
+        <Route path='/login' exact element={<LogIn/>} />
+        <Route path='/user/sales' exact element={<Usersales/>} />
+        <Route path='/user/allsales' exact element={<SalesComponent/>} />
+        <Route path='/user/showgraph' exact element={<UserSalesGraph/>} />
+        <Route path='/user' exact element={<UserNavbar/>} />
+      </Routes>
+
       <FooterComponent />
-    </div>
+    </Router>
   );
 }
-
-export default App;
