693751bb7ba1726c8e8edec0fcc1f65176fa1804
prateeksIngh508 <prateek.singh508@gmail.com>
2022-07-27 12:41:41 +0530

    add saleman and salesman list

--- simp-frontend/src/App.js
+++ simp-frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+<<<<<<< Updated upstream
 function App() {
   return (
     <div className="App">
@@ -19,6 +20,38 @@ function App() {
         </a>
       </header>
     </div>
+=======
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import LogIn from './components/LogIn';
+import UserNavbar from './components/UserNavbar';
+import SalesComponent from './components/SalesComponent';
+import Usersales from './components/Usersales';
+import UserSalesGraph from './components/UserSalesGraph';
+import AdminNavbar from './components/AdminNavbar';
+import SalesmanList from './components/SalesmenList';
+import AddSalesman from './components/AddSalesman';
+
+export default function App() {
+  return (
+    <Router>
+      <HeaderComponent />
+      
+      <Routes>
+        <Route path='/' exact element={<LogIn/>} />
+        <Route path='/login' exact element={<LogIn/>} />
+        <Route path='/user/sales' exact element={<Usersales/>} />
+        <Route path='/user/allsales' exact element={<SalesComponent/>} />
+        <Route path='/user/showgraph' exact element={<UserSalesGraph/>} />
+        <Route path='/user' exact element={<UserNavbar/>} />
+        <Route path='/admin' exact element={<AdminNavbar/>} />
+        <Route path='/admin/salesmen-list' exact element={<SalesmanList/>} />
+        <Route path='/admin/add-salesman' exact element={<AddSalesman/>} />
+      </Routes>
+
+      <FooterComponent />
+    </Router>
+>>>>>>> Stashed changes
   );
 }
 
