59505a00a80f76f21ef29f693da3fd5e7757a726
kumarsaksham <64663891+kumarsaksham@users.noreply.github.com>
2022-07-28 02:31:49 +0530

    created userSales component

--- simp-frontend/src/services/UserService.js
+++ simp-frontend/src/services/UserService.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
-const BASIC_URL = "http://localhost:8080/user/allsales"
+const BASIC_URL = "http://localhost:8080"
 
 class UserService {
      
     getSalesDetails(){
-        return axios.get(BASIC_URL);
+        return axios.get(`${BASIC_URL}/user/allsales`);
+    }
+
+    getMySales(userId, transactionMonth){
+        return axios.get(`${BASIC_URL}/user/mysales?userId=${userId}&transactionMonth="${transactionMonth}"`);
+       
     }
 }
 
