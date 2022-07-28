777c1096940e2569140b19a507ed73d74067b64e
tarun gowda <tarun.gowda2000@gmail.com>
2022-07-28 12:05:55 +0530

    drop csv file

---
+++ simp-frontend/src/components/ChangeCommisionStructure.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import AdminNavbar from './AdminNavbar'
+
+export default class ChangeCommisionStructure extends Component {
+  render() {
+    return (
+        <div>
+            <AdminNavbar />
+            <div>ChangeCommisionStructure</div>
+        </div>
+    )
+  }
+}
---
+++ simp-frontend/src/components/ForcedCommissionCalculation.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import AdminNavbar from './AdminNavbar'
+
+export default class ForcedCommissionCalculation extends Component {
+  render() {
+    return (
+        <div>
+            <AdminNavbar />
+            <div>ForcedCommissionCalculation</div>
+        </div>
+    )
+  }
+}
