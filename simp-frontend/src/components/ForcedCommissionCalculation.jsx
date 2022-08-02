a4473e47643428cd2bbe8a7c5e07f6c6cfdbda8c
kumarsaksham <64663891+kumarsaksham@users.noreply.github.com>
2022-08-02 23:40:54 +0530

    forcedCommision connection

--- simp-frontend/src/components/ForcedCommissionCalculation.jsx
+++ simp-frontend/src/components/ForcedCommissionCalculation.jsx
@@ -1,42 +1,93 @@
 import React, { Component } from 'react'
 import AdminNavbar from './AdminNavbar'
+import UserService from '../services/UserService'
 
 export default class ForcedCommissionCalculation extends Component {
-  render() {
-    return (
-        <div>
-            <AdminNavbar />
-            <div className=" container col-sm-6 offset-md-4 mt-5 mb-2">
-            <h2 className="primary"> Forced Commission </h2>
-            <form>
-            <div className="row mb-3 mt-4">
-                    <label className="col-sm-2 col-form-label">User Id:</label>
-                    <div className="col-sm-4">
-                        <input type="text" className ="form-control" id = "userId"  name="userId" />
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+          userId: null,
+          userAmount : 0,
+          month: ""
+        }
+    
+        this.handleUserIdChange = (event) => {
+          this.setState({
+            userId: event.target.value
+          })
+        }
+
+        this.handleMonthChange = (event) => {
+            this.setState({
+              month: event.target.value
+            })
+        }
+
+        this.handleCommissionChange = (event) => {
+            this.setState({
+                userAmount: event.target.value
+            })
+        }
+
+        this.saveCommisssion = (event) => {
+            event.preventDefault();
+            const commission = {userId: this.state.userId, month: this.state.month, userAmount: this.state.userAmount};
+            UserService.updateCommission(this.state.userId, this.state.month, commission)
+            .then((res) => {
+                // console.log(res.data);
+                alert(`Commission updated for user id: ${this.state.userId}`);
+                // const userId = this.state.userId;
+                // const userAmount = this.state.userAmount;
+                // const month = this.state.month;
+                // this.props.onSearchTermChange(userId, userAmount, month);
+                // this.setState({
+                //     userId: null,
+                //     userAmount : 0,
+                //     month: ""
+                // });
+            })
+            .catch(err => console.log(err))
+
+            
+        } 
+    }
+
+    render() {
+        return (
+            <div>
+                <AdminNavbar />
+                <div className=" container col-sm-6 offset-md-4 mt-5 mb-2">
+                <h2 className="primary"> Forced Commission </h2>
+                <form onSubmit={this.saveCommisssion}>
+                <div className="row mb-3 mt-4">
+                        <label className="col-sm-2 col-form-label">User Id:</label>
+                        <div className="col-sm-4">
+                            <input type="text" className ="form-control" id = "userId"  name="userId" onChange={this.handleUserIdChange}/>
+                        </div>
+                        
+                    </div> 
+                    <div className="row mb-3">
+                        <label className="col-sm-2 col-form-label">Month :</label>
+                        <div className="col-sm-4">
+                            <input type="month" className ="form-control" id = "month"  name="month" onChange={this.handleMonthChange}/>
+                        </div>
+                        
+                    </div> 
+                    
+                    <div className="row mb-3">
+                        <label className="col-sm-2 col-form-label">Commisson:</label>
+                        <div className="col-sm-4">
+                            <input type="text" className ="form-control" id = "userAmount" name="userAmount" onChange={this.handleCommissionChange}/>
+                        </div>
+                        
                     </div>
                     
-                </div> 
-                <div className="row mb-3">
-                    <label className="col-sm-2 col-form-label">Month :</label>
-                    <div className="col-sm-4">
-                        <input type="text" className ="form-control" id = "month"  name="month"/>
-                    </div>
-                    
-                </div> 
-                
-                <div className="row mb-3">
-                    <label className="col-sm-2 col-form-label">Commisson:</label>
-                    <div className="col-sm-4">
-                        <input type="text" className ="form-control" id = "userAmount" name="userAmount" />
-                    </div>
-                    
-                </div>
-                
-                <button className = "btn btn-primary col-sm-6" onClick={this.saveCommisssion}>Update</button>
-      
-            </form>
-        </div>
-        </div>
-    )
-  }
+                    <button className = "btn btn-primary col-sm-6" type='submit'>Update</button>
+            
+                </form>
+            </div>
+            </div>
+        )
+    }
 }
